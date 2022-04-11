9d9778d187a075205570c0391b9898ccc8c7f8eb
Sirish Bandi <sirishbandi@google.com>
2022-04-11 21:19:52 +0530

    fixed

--- script.sh
+++ script.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 echo "Coverting Image"
-convert channel.png -dither FloydSteinberg -define dither:diffusion-amount=85% -monochrome -colors 2 channel.bmp
-
-echo "Files"
-ls -l
+convert channel.png -dither FloydSteinberg -define dither:diffusion-amount=85% -monochrome -colors 2 -negate channel.bmp
 
 echo "Uploading Image"
 gsutil -o "GSUtil:parallel_process_count=1" cp channel.bmp gs://keychainbucket/
