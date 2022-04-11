61e830a3acd68fab5ace21ae65571d770456d4c4
Sirish Bandi <sirishbandi@google.com>
2022-04-11 18:10:13 +0530

    mem limt

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ echo "Coverting Image"
 convert channel.png -dither FloydSteinberg -define dither:diffusion-amount=85% -monochrome -colors 2 channel.bmp
 
 echo "Uploading Image"
-gsutil cp channel.bmp gs://keychainbucket/
+gsutil -o "GSUtil:parallel_process_count=1" cp channel.bmp gs://keychainbucket/
